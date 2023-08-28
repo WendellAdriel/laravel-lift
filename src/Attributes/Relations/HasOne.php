4fd61f40fcaf140d9eb492fb46e6e67c205e2bca
Wendell Adriel <wendelladriel.ti@gmail.com>
2023-08-28 17:20:04 +0100

    Created Relations attributes classes

---
+++ src/Attributes/Relations/HasOne.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WendellAdriel\Lift\Attributes\Relations;
+
+use Attribute;
+use Illuminate\Support\Str;
+use WendellAdriel\Lift\Concerns\HasArguments;
+use WendellAdriel\Lift\Contracts\RelationAttribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+final class HasOne implements RelationAttribute
+{
+    use HasArguments;
+
+    /**
+     * @var class-string
+     */
+    public string $relationClass;
+
+    /**
+     * @var array<mixed>
+     */
+    public array $arguments = [];
+
+    /**
+     * @param  class-string  $relationClass
+     * @param  array<mixed>  ...$arguments
+     */
+    public function __construct(string $relationClass, array ...$arguments)
+    {
+        $this->relationClass = $relationClass;
+        $this->arguments = [$relationClass, ...$arguments];
+    }
+
+    public function relationName(): string
+    {
+        return Str::singular(mb_strtolower(class_basename($this->relationClass)));
+    }
+}
